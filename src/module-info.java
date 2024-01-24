59183a1633e9fabbddb90b01c8e4edc8829493b1
DanielCHFer <danielchfer@gmail.com>
2024-01-24 22:35:35 +0100

    Acceso a la API
    Se accede a la API de Marvel con la clave de usuario y se leen todas las apariciones del personaje, guardándolas en una nueva clase Comic

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,9 @@
  * 
  */
 module ProyectoEvaluacionFinal {
+	requires java.desktop;
+	requires org.apache.commons.codec;
+	requires org.apache.httpcomponents.httpclient;
+	requires org.apache.httpcomponents.httpcore;
+	requires org.json;
 }
